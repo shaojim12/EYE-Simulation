80ef59f457d7091b4fec480fca36583ced59873b
Nei-Chun Shao <ns948@cornell.edu>
2021-11-08 21:51:52 -0500

    add serial

--- serial_input.py
+++ serial_input.py
@@ -1,16 +1,19 @@
 import serial
 
-ser=serial.Serial("/dev/ttyACM0", 9600)
-ser.baudrate=9600
+
+class Serial_input:
+    def __init__(self, baud = 9600):
+        self.ser = serial.Serial("/dev/ttyACM0", baud)
+        self.ser.baudrate = baud
 
 while(True):
         
-    print("start sound detect while loop")
-    MIC_VAL= ser.readline()
-    print(MIC_VAL)
+    # print("start sound detect while loop")
+    input1= ser.readline()
+    # print(MIC_VAL)
 
     #print(MIC_VAL.decode('utf-8'))
-    temp_read = MIC_VAL.decode('utf-8')
+    temp_read = input1.decode('utf-8')
     temp_read = temp_read.strip()
     print(temp_read)
     
